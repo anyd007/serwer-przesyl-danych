ba68911878428c497830a4f969509fd3f20f4069
Andrzej Gębala <gebala.andrzej1@gmial.com>
2022-02-26 10:33:08 +0100

    stworzony serwer przy pomocy express js

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express")
+const cors = require("cors")
+const server = {
+    port:1234
+}
+const app = express()
+
+const regystryUsers = []
+const loginUserDatabase = []
+app.use(cors())
+app.use(express.json())
+
+
+//przekazywania danych na stronę sewera
+app.get("/",(req,res) =>{
+    app.send(req.body)
+})
+
+// pobieranie danych z rejestracji i zapisywanie ich do tablicy regystryUsers
+app.post("/regestry", (req,res)=>{
+    regystryUsers.push(req.body)
+    res.status(200).end
+})
+// pobieranie danych z inputów dream teamu i dodawanie ich do pustej tablict "loginUserDatabase"
+app.post("/loginUserDatabase", (req,res)=>{
+    loginUserDatabase.push(req.body)
+    res.status(200).end
+})
+// wysłanie danych z rejestracji zapisanych na serwerze z powrotem juz na strone logowania do sprawdzenia poprawnosci logowania usera
+app.get("/regestry", (req,res)=>{
+    res.json({regystryUsers})
+})
+   //wysłanie danych do bazy danych zalogowanego uzytkownika
+   app.get("/loginUserDatabase",(req,res)=>{
+    res.json({loginUserDatabase})
+})
+
+app.listen(server.port, ()=>{
+    console.log(`Działam na porcie ${server.port}`);
+})
