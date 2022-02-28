3c2f9bd4d92b9a9b478ff800eb419b51157e68e9
Andrzej Gębala <gebala.andrzej1@gmial.com>
2022-02-28 11:07:45 +0100

    problemy z cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var expressWs = require('express-ws')(app);
 //  }
 // app.use(cors(corsOptions));
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://dream-team-andrzej.herokuapp.com/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "https://dream-team-andrzej.herokuapp.com"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
