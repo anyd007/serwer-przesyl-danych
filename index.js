db0dc372496dd736c835c11e1c19ad7d591053dd
Andrzej Gębala <gebala.andrzej1@gmial.com>
2022-02-28 09:14:13 +0100

    problemy z cors

--- index.js
+++ index.js
@@ -1,56 +1,54 @@
-const express = require("express")
-var cors = require('cors')
-const path = require("path/posix")
-// const server = {
-//     port:5000
-// }
-const app = express()
-
-const regystryUsers = []
-const loginUserDatabase = []
-
-app.use(cors());
-app.use(express.json())
-
-var whitelist = ['https://dream-team-andrzej.herokuapp.com', 'http://localhost:3000']
+const express = require("express");
+var cors = require("cors");
+const path = require("path/posix");
+const app = express();
+var whitelist = [
+  "https://dream-team-andrzej.herokuapp.com",
+  "http://localhost:3000",
+];
 var corsOptions = {
-    credentials: true,
-    origin: function(origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
-        callback(null, true)
-      } else {
-        callback(new Error('Not allowed by CORS'))
-      }
+  credentials: true,
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
     }
-  }
-  
+  },
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+const regystryUsers = [];
+
+const loginUserDatabase = [];
+
 //przekazywania danych na stronę sewera
-app.get("/",(req,res) =>{
-    res.send(req.body)
-})
+app.get("/", (req, res) => {
+  res.send(req.body);
+});
 
 // pobieranie danych z rejestracji i zapisywanie ich do tablicy regystryUsers
-app.post("/regestry", cors(corsOptions),(req,res)=>{
-    regystryUsers.push(req.body)
-    res.status(200).end
-})
+app.post("/regestry", (req, res) => {
+  regystryUsers.push(req.body);
+  res.status(200).end;
+});
 // pobieranie danych z inputów dream teamu i dodawanie ich do pustej tablict "loginUserDatabase"
-app.post("/loginUserDatabase", cors(corsOptions),(req,res)=>{
-    loginUserDatabase.push(req.body)
-    res.status(200).end
-})
+app.post("/loginUserDatabase", (req, res) => {
+  loginUserDatabase.push(req.body);
+  res.status(200).end;
+});
 // wysłanie danych z rejestracji zapisanych na serwerze z powrotem juz na strone logowania do sprawdzenia poprawnosci logowania usera
-app.get("/regestry", cors(corsOptions), (req,res)=>{
-    res.json({regystryUsers})
-})
-   //wysłanie danych do bazy danych zalogowanego uzytkownika
-   app.get("/loginUserDatabase", cors(corsOptions), (req,res)=>{
-    res.json({loginUserDatabase})
-})
+app.get("/regestry", (req, res) => {
+  res.json({ regystryUsers });
+});
+//wysłanie danych do bazy danych zalogowanego uzytkownika
+app.get("/loginUserDatabase", (req, res) => {
+  res.json({ loginUserDatabase });
+});
 
-//tworzenie zmiennej która przekaże dane do heroku, dodatkowo należy dopisać w package.jeson w scripts : "web": "index.js"  
-const herokuPort = process.env.PORT || 5000
+//tworzenie zmiennej która przekaże dane do heroku, dodatkowo należy dopisać w package.jeson w scripts : "web": "index.js"
+const herokuPort = process.env.PORT || 5000;
 //nasłuchiwanie app na jakim porcie na działać
-app.listen(herokuPort, ()=>{ 
-    console.log(`Działam na porcie ${herokuPort}`);
-})
+app.listen(herokuPort, () => {
+  console.log(`Działam na porcie ${herokuPort}`);
+});
