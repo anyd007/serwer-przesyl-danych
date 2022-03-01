418e301d79b4308e514d5d43571af15abf35e02c
Andrzej Gębala <gebala.andrzej1@gmial.com>
2022-03-01 17:38:18 +0100

    zmiany w organizacji pracy heroku.

--- index.js
+++ index.js
@@ -7,21 +7,21 @@ app.use('/', express.static(__dirname +'/src'));
 app.use('/public', express.static(__dirname +'/public'));
 app.use(cors());
 var whitelist = ['https://dream-team-andrzej.herokuapp.com','http://localhost:3000']
-// var corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true)
-//     } else {
-//       callback(new Error('Not allowed by CORS'))
-//     }
-//   }
-// }
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
 app.use(express.json());
 const regestryUsers = [];
 const loginUserDatabase = [];
 
 //przekazywania danych na stronę sewera
-app.get("/", cors(), (req, res) => {
+app.get("/", cors(corsOptions), (req, res) => {
   res.send(req.body);
 });
 
@@ -36,11 +36,11 @@ app.post("/api/loginUserDatabase", (req, res) => {
   res.status(200).end;
 });
 // wysłanie danych z rejestracji zapisanych na serwerze z powrotem juz na strone logowania do sprawdzenia poprawnosci logowania usera
-app.get("/api/regestry", cors(),(req, res) => {
+app.get("/api/regestry", cors(corsOptions),(req, res) => {
   res.json({ regestryUsers });
 });
 //wysłanie danych do bazy danych zalogowanego uzytkownika
-app.get("/api/loginUserDatabase", cors(), (req, res) => {
+app.get("/api/loginUserDatabase", cors(corsOptions), (req, res) => {
   res.json({ loginUserDatabase });
 });
 //tworzenie zmiennej która przekaże dane do heroku, dodatkowo należy dopisać w package.jeson w scripts : "web": "index.js"
