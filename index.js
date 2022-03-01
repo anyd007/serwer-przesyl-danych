6791e15e1d7c6e0f30105b2322db18398ec3601d
Andrzej Gębala <gebala.andrzej1@gmial.com>
2022-03-01 15:56:14 +0100

    testy na nowej branczy

--- index.js
+++ index.js
@@ -3,21 +3,16 @@ const express = require("express");
 var cors = require("cors");
 const path = require("path/posix");
 const app = express();
-const multipleCORSDomain = require('multiple-cors-domain');
-// const corsOptions ={
-//     origin:'*', 
-//     credentials:true,            
-//     optionSuccessStatus:200,
-//  }
-// app.use(cors(corsOptions));
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://dream-team-andrzej.herokuapp.com"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-  app.use(multipleCORSDomain({ any: true }));
+app.use('/', express.static(__dirname +'/src'));
+app.use('/public', express.static(__dirname +'/public'));
+app.use(cors());
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "https://dream-team-andrzej.herokuapp.com"); // update to match the domain you will make the request from
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
 app.use(express.json());
-const regystryUsers = [];
+const regestryUsers = [];
 const loginUserDatabase = [];
 
 //przekazywania danych na stronę sewera
@@ -26,24 +21,26 @@ app.get("/", (req, res) => {
 });
 
 // pobieranie danych z rejestracji i zapisywanie ich do tablicy regystryUsers
-app.post("/regestry",(req, res) => {
-  regystryUsers.push(req.body);
+app.post("/api/regestry",(req, res) => {
+  regestryUsers.push(req.body);
   res.status(200).end;
 });
 // pobieranie danych z inputów dream teamu i dodawanie ich do pustej tablict "loginUserDatabase"
-app.post("/loginUserDatabase", (req, res) => {
+app.post("/api/loginUserDatabase", (req, res) => {
   loginUserDatabase.push(req.body);
   res.status(200).end;
 });
 // wysłanie danych z rejestracji zapisanych na serwerze z powrotem juz na strone logowania do sprawdzenia poprawnosci logowania usera
-app.get("/regestry",(req, res) => {
-  res.json({ regystryUsers });
+app.get("/api/regestry",(req, res) => {
+  res.json({ regestryUsers });
 });
 //wysłanie danych do bazy danych zalogowanego uzytkownika
-app.get("/loginUserDatabase", (req, res) => {
+app.get("/api/loginUserDatabase", (req, res) => {
   res.json({ loginUserDatabase });
 });
-
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  });
 //tworzenie zmiennej która przekaże dane do heroku, dodatkowo należy dopisać w package.jeson w scripts : "web": "index.js"
 const herokuPort = process.env.PORT || 5000;
 //nasłuchiwanie app na jakim porcie na działać
