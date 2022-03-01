f66a862ec61419bfac22af7908907c976cf9e654
Andrzej Gębala <gebala.andrzej1@gmial.com>
2022-03-01 17:54:47 +0100

    zmiany w organizacji pracy heroku.

--- index.js
+++ index.js
@@ -6,22 +6,26 @@ const app = express();
 app.use('/', express.static(__dirname +'/src'));
 app.use('/public', express.static(__dirname +'/public'));
 app.use(cors());
-var whitelist = ['https://dream-team-andrzej.herokuapp.com/api/regestry','https://dream-team-andrzej.herokuapp.com/api/loginUserDatabase','http://localhost:3000']
-var corsOptionsDelegate = function (req, callback) {
-  var corsOptions;
-  if (whitelist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false } // disable CORS for this request
-  }
-  callback(null, corsOptions) // callback expects two parameters: error and options
+// var whitelist = ['https://dream-team-andrzej.herokuapp.com','http://localhost:3000']
+// var corsOptionsDelegate = function (req, callback) {
+//   var corsOptions;
+//   if (whitelist.indexOf(req.header('Origin')) !== -1) {
+//     corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+//   } else {
+//     corsOptions = { origin: false } // disable CORS for this request
+//   }
+//   callback(null, corsOptions) // callback expects two parameters: error and options
+// }
+var corsOptions = {
+  origin: 'https://dream-team-andrzej.herokuapp.com',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 app.use(express.json());
 const regestryUsers = [];
 const loginUserDatabase = [];
 
 //przekazywania danych na stronę sewera
-app.get("/", cors(corsOptionsDelegate), (req, res) => {
+app.get("/", cors(corsOptions), (req, res) => {
   res.send(req.body);
 });
 
@@ -36,11 +40,11 @@ app.post("/api/loginUserDatabase", (req, res) => {
   res.status(200).end;
 });
 // wysłanie danych z rejestracji zapisanych na serwerze z powrotem juz na strone logowania do sprawdzenia poprawnosci logowania usera
-app.get("/api/regestry", cors(corsOptionsDelegate),(req, res) => {
+app.get("/api/regestry", cors(corsOptions),(req, res) => {
   res.json({ regestryUsers });
 });
 //wysłanie danych do bazy danych zalogowanego uzytkownika
-app.get("/api/loginUserDatabase", cors(corsOptionsDelegate), (req, res) => {
+app.get("/api/loginUserDatabase", cors(corsOptions), (req, res) => {
   res.json({ loginUserDatabase });
 });
 //tworzenie zmiennej która przekaże dane do heroku, dodatkowo należy dopisać w package.jeson w scripts : "web": "index.js"
