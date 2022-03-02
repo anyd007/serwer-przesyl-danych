924f6ed5341ad9d32742290bad96b1406c7ef7b2
Andrzej Gębala <gebala.andrzej1@gmial.com>
2022-03-02 13:40:24 +0100

    cors...

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ var cors = require("cors");
 const path = require("path");
 const app = express();
 // app.use(cors());
-const whitelist = ["http://localhost:3000","https://serwer-dream-team.herokuapp.com"]
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error("Not allowed by CORS"))
-    }
-  },
-  credentials: true,
-}
-app.use(cors(corsOptions))
+// const whitelist = ["http://localhost:3000","https://serwer-dream-team.herokuapp.com"]
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (!origin || whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error("Not allowed by CORS"))
+//     }
+//   },
+//   credentials: true,
+// }
+// app.use(cors(corsOptions))
 
 
 // app.use('/', express.static(__dirname +'/src'));
