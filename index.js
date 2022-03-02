63021e1187909e6c70207dacc17dc3fbcf9fb25c
Andrzej Gębala <gebala.andrzej1@gmial.com>
2022-03-02 12:00:31 +0100

    cors...

--- index.js
+++ index.js
@@ -22,26 +22,26 @@ const regestryUsers = [];
 const loginUserDatabase = [];
 
 //przekazywania danych na stronę sewera
-app.get("*",cors(),(req, res) => {
+app.get("/",cors(),(req, res) => {
   res.send(req.body);
 });
 
 // pobieranie danych z rejestracji i zapisywanie ich do tablicy regystryUsers
-app.post("api/regestry",(req, res) => {
+app.post("/api/regestry",(req, res) => {
   regestryUsers.push(req.body);
   res.status(200).end;
 });
 // pobieranie danych z inputów dream teamu i dodawanie ich do pustej tablict "loginUserDatabase"
-app.post("api/loginUserDatabase",(req, res) => {
+app.post("/api/loginUserDatabase",(req, res) => {
   loginUserDatabase.push(req.body);
   res.status(200).end;
 });
 // wysłanie danych z rejestracji zapisanych na serwerze z powrotem juz na strone logowania do sprawdzenia poprawnosci logowania usera
-app.get("api/regestry",cors(), (req, res) => {
+app.get("/api/regestry",cors(), (req, res) => {
   res.json({ regestryUsers });
 });
 //wysłanie danych do bazy danych zalogowanego uzytkownika
-app.get("api/loginUserDatabase", cors(),(req, res) => {
+app.get("/api/loginUserDatabase", cors(),(req, res) => {
   res.json({ loginUserDatabase });
 });
 //tworzenie zmiennej która przekaże dane do heroku, dodatkowo należy dopisać w package.jeson w scripts : "web": "index.js"
