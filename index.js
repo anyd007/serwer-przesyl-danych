0131fe1ec3be671f780c4ec415199911a1aafffe
Andrzej Gębala <gebala.andrzej1@gmial.com>
2022-03-02 11:07:28 +0100

    cors...

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const regestryUsers = [];
 const loginUserDatabase = [];
 
 //przekazywania danych na stronę sewera
-app.get("/",(req, res) => {
+app.get("/",cors(),(req, res) => {
   res.send(req.body);
 });
 
@@ -30,11 +30,11 @@ app.post("/api/loginUserDatabase",(req, res) => {
   res.status(200).end;
 });
 // wysłanie danych z rejestracji zapisanych na serwerze z powrotem juz na strone logowania do sprawdzenia poprawnosci logowania usera
-app.get("/api/regestry",(req, res) => {
+app.get("/api/regestry",cors(), (req, res) => {
   res.json({ regestryUsers });
 });
 //wysłanie danych do bazy danych zalogowanego uzytkownika
-app.get("/api/loginUserDatabase", (req, res) => {
+app.get("/api/loginUserDatabase", cors(),(req, res) => {
   res.json({ loginUserDatabase });
 });
 //tworzenie zmiennej która przekaże dane do heroku, dodatkowo należy dopisać w package.jeson w scripts : "web": "index.js"
