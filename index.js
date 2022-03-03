901cee09cdc986a6f9d2690f7a6e671036de34c0
Andrzej Gębala <gebala.andrzej1@gmial.com>
2022-03-03 20:20:38 +0100

    poprawa stautsu 503

--- index.js
+++ index.js
@@ -1,39 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-app.use(cors())
+const corsOptions = {
+  origin: process.env.CORS_ALLOW_ORIGIN || '*',
+  methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
+app.use(cors(corsOptions)); 
 const path = require("path");
 const asyncHandler = require("express-async-handler");
 require("express-async-errors");
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "*");
-
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
-  next();
-});
-
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   next();
-// });
-
+ 
 app.use("/", express.static(__dirname + "src"));
 app.use("/public", express.static(__dirname + "public"));
 
