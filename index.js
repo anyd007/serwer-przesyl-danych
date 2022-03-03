f4707a98cd362d199b217d201bed8cedc9ffed9d
Andrzej Gębala <gebala.andrzej1@gmial.com>
2022-03-04 00:26:53 +0100

    poprawa stautsu cors

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-app.use(cors())
+// app.use(cors())
 const path = require("path");
+const asyncHandler = require("express-async-handler");
+require("express-async-errors");
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
-const { createProxyMiddleware } = require('http-proxy-middleware');
-app.use('/api/regestry', createProxyMiddleware({ 
-  target: 'https://serwer-dream-team.herokuapp.com/', //original url
-  changeOrigin: true, 
-  //secure: false,
-  onProxyRes: function (proxyRes, req, res) {
-     proxyRes.headers['Access-Control-Allow-Origin'] = '*';
-  }
-}));
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
 
-// app.use("/", express.static(__dirname + "src"));
-// app.use("/public", express.static(__dirname + "public"));
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   next();
+// });
+
+app.use("/", express.static(__dirname + "src"));
+app.use("/public", express.static(__dirname + "public"));
 
 app.use(express.json());
 const regestryUsers = [];
