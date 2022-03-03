5c1d5487e245157ae431a2cf93b0ddb65d591d5c
Andrzej Gębala <gebala.andrzej1@gmial.com>
2022-03-03 20:11:59 +0100

    poprawa stautsu 503

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-// app.use(cors())
+app.use(cors())
 const path = require("path");
 const asyncHandler = require("express-async-handler");
 require("express-async-errors");
